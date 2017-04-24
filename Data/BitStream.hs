ec4b3e4f8fa3ffdae588ca61339893231a9535e0
Bodigrim <andrew.lelechenko@gmail.com>
2017-04-25 00:20:15 +0300

    Comment functions

--- Data/BitStream.hs
+++ Data/BitStream.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module:      Data.BitStream
+-- Copyright:   (c) 2017 Andrew Lelechenko
+-- Licence:     MIT
+-- Maintainer:  Andrew Lelechenko <andrew.lelechenko@gmail.com>
+--
+-- Lazy, infinite, compact stream of 'Bool' with O(1) indexing.
+-- Most useful for memoization of predicates.
+
 module Data.BitStream
   ( BitStream
   , tabulate
@@ -11,6 +20,19 @@ import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector as V
 import Unsafe.Coerce
 
+-- | Compact representation of infinite stream of 'Bool'.
+--
+-- It spends one bit (1/8 byte) for one 'Bool' in store.
+-- Compare it to at least 24 bytes per element in @[Bool]@,
+-- approximately 2 bytes per element in 'IntSet'
+-- and 1 byte per element in unboxed @Vector Bool@.
+--
+-- It also offers indexing in constant time.
+-- Compare it to linear time for lists and logarithmic time for sets.
+--
+-- Moreover, it is lazy: querying n-th element triggers computation
+-- of first @max(64, 2 ^ ceiling (logBase 2 n))@ elements only. On contrary,
+-- sets and unboxed vectors are completely strict.
 newtype BitStream = BitStream { _unBitStream :: V.Vector (U.Vector Word) }
 
 word2int :: Word -> Int
@@ -25,6 +47,9 @@ bits = finiteBitSize (0 :: Word)
 bitsLog :: Int
 bitsLog = bits - 1 - countLeadingZeros (int2word bits)
 
+-- | Create a bit stream from the predicate.
+-- The predicate must be well-defined for any value of argument
+-- and should not return 'error'/'undefined'.
 tabulate :: (Word -> Bool) -> BitStream
 tabulate f = BitStream $ U.singleton (tabulateW 0) `V.cons` V.generate (bits - bitsLog) tabulateU
   where
@@ -38,6 +63,9 @@ tabulate f = BitStream $ U.singleton (tabulateW 0) `V.cons` V.generate (bits - b
       where
         jj = j `shiftL` bitsLog
 
+-- | Convert bit stream back to predicate.
+-- Indexing itself works in O(1) time, but triggers evaluation and allocation
+-- of surrounding elements of the stream, if they were not computed before.
 index :: BitStream -> Word -> Bool
 index (BitStream vus) i =
   if sgm < 0 then indexU (V.unsafeHead vus) (word2int i)
