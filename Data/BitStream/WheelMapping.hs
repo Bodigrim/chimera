73b5020ba01fce4589efcde32144ef2c91e9686d
Bodigrim <andrew.lelechenko@gmail.com>
2017-07-13 22:38:22 +0300

    Fix monotonicity of toWheel*

--- Data/BitStream/WheelMapping.hs
+++ Data/BitStream/WheelMapping.hs
@@ -27,7 +27,7 @@ import Data.Word
 word2int :: Word -> Int
 word2int = fromIntegral
 
--- | Left inverse for 'fromWheel2'.
+-- | Left inverse for 'fromWheel2'. Monotonically non-decreasing function.
 --
 -- prop> toWheel2 . fromWheel2 == id
 toWheel2 :: Word -> Word
@@ -45,7 +45,7 @@ fromWheel2 :: Word -> Word
 fromWheel2 i = i `shiftL` 1 + 1
 {-# INLINE fromWheel2 #-}
 
--- | Left inverse for 'fromWheel6'.
+-- | Left inverse for 'fromWheel6'. Monotonically non-decreasing function.
 --
 -- prop> toWheel6 . fromWheel6 == id
 toWheel6 :: Word -> Word
@@ -63,7 +63,7 @@ fromWheel6 :: Word -> Word
 fromWheel6 i = i `shiftL` 1 + i + (i .&. 1) + 1
 {-# INLINE fromWheel6 #-}
 
--- | Left inverse for 'fromWheel30'.
+-- | Left inverse for 'fromWheel30'. Monotonically non-decreasing function.
 --
 -- prop> toWheel30 . fromWheel30 == id
 toWheel30 :: Word -> Word
@@ -94,7 +94,7 @@ toWheel210 i = q * 48 + fromIntegral (toWheel210Table `U.unsafeIndex` word2int r
 {-# INLINE toWheel210 #-}
 
 toWheel210Table :: U.Vector Word8
-toWheel210Table = U.fromList [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 4, 0, 0, 0, 5, 0, 0, 0, 0, 0, 6, 0, 7, 0, 0, 0, 0, 0, 8, 0, 0, 0, 9, 0, 10, 0, 0, 0, 11, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 13, 0, 14, 0, 0, 0, 0, 0, 15, 0, 0, 0, 16, 0, 17, 0, 0, 0, 0, 0, 18, 0, 0, 0, 19, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 22, 0, 23, 0, 0, 0, 24, 0, 25, 0, 0, 0, 26, 0, 0, 0, 0, 0, 0, 0, 27, 0, 0, 0, 0, 0, 28, 0, 0, 0, 29, 0, 0, 0, 0, 0, 30, 0, 31, 0, 0, 0, 32, 0, 0, 0, 0, 0, 33, 0, 34, 0, 0, 0, 0, 0, 35, 0, 0, 0, 0, 0, 36, 0, 0, 0, 37, 0, 38, 0, 0, 0, 39, 0, 0, 0, 0, 0, 40, 0, 41, 0, 0, 0, 0, 0, 42, 0, 0, 0, 43, 0, 44, 0, 0, 0, 45, 0, 46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47]
+toWheel210Table = U.fromList [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12, 12, 12, 13, 13, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 16, 16, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 22, 22, 23, 23, 23, 23, 24, 24, 25, 25, 25, 25, 26, 26, 26, 26, 26, 26, 26, 26, 27, 27, 27, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 29, 29, 29, 30, 30, 31, 31, 31, 31, 32, 32, 32, 32, 32, 32, 33, 33, 34, 34, 34, 34, 34, 34, 35, 35, 35, 35, 35, 35, 36, 36, 36, 36, 37, 37, 38, 38, 38, 38, 39, 39, 39, 39, 39, 39, 40, 40, 41, 41, 41, 41, 41, 41, 42, 42, 42, 42, 43, 43, 44, 44, 44, 44, 45, 45, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 47]
 
 -- | 'fromWheel210' n is the (n+1)-th positive number, not divisible by 2, 3, 5 or 7.
 -- Sequence <https://oeis.org/A008364 A008364>.
