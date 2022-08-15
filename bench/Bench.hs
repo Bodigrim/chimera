503564de077ce858854a6932bcdadd9075d1c405
Bodigrim <andrew.lelechenko@gmail.com>
2022-08-15 20:07:45 +0100

    Tweak benchmarks to spend less time in division

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import Control.Monad.State (evalState, put, get)
+import Data.Bits
 import Data.Chimera
 import Test.Tasty.Bench
 import System.Random
@@ -12,7 +13,7 @@ import qualified Data.RAList as RAL
 #endif
 
 sizes :: Num a => [a]
-sizes = [100, 200, 500, 1000]
+sizes = [7, 8, 9, 10]
 
 main :: IO ()
 main = defaultMain $ (: []) $ bgroup "read"
@@ -35,7 +36,7 @@ randomList = randoms (mkStdGen 42)
 
 #ifdef MIN_VERSION_ral
 randomRAL :: RAL.RAList Int
-randomRAL = RAL.fromList $ take (maximum sizes) $ randoms (mkStdGen 42)
+randomRAL = RAL.fromList $ take (1 `shiftL` (maximum sizes)) $ randoms (mkStdGen 42)
 #endif
 
 randomIndicesWord :: [Word]
@@ -44,27 +45,33 @@ randomIndicesWord = randoms (mkStdGen 42)
 randomIndicesInt :: [Int]
 randomIndicesInt = randoms (mkStdGen 42)
 
-benchReadChimera :: Word -> Benchmark
-benchReadChimera n
+benchReadChimera :: Int -> Benchmark
+benchReadChimera k
   = bench (show n)
   $ nf (sum . map (index randomChimera))
-  $ map (`rem` n)
+  $ map (.&. (n - 1))
   $ take (fromIntegral n) randomIndicesWord
+  where
+    n = 1 `shiftL` k
 
 benchReadList :: Int -> Benchmark
-benchReadList n
+benchReadList k
   = bcompare ("$NF == \"" ++ show n ++ "\" && $(NF-1) == \"Chimera\"")
   $ bench (show n)
   $ nf (sum . map (randomList !!))
-  $ map (`mod` n)
+  $ map (.&. (n - 1))
   $ take n randomIndicesInt
+  where
+    n = 1 `shiftL` k
 
 #ifdef MIN_VERSION_ral
 benchReadRAL :: Int -> Benchmark
-benchReadRAL n
+benchReadRAL k
   = bcompare ("$NF == \"" ++ show n ++ "\" && $(NF-1) == \"Chimera\"")
   $ bench (show n)
   $ nf (sum . map (randomRAL RAL.!))
-  $ map (`mod` n)
+  $ map (.&. (n - 1))
   $ take n randomIndicesInt
+  where
+    n = 1 `shiftL` k
 #endif
